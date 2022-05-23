0a91fe2e346077b3b8b305853f784000bcca8040
Oleksandr Melnychuk <alex.melnychuk28@gmail.com>
2022-05-23 21:04:36 +0300

    todo.ts class has been implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Todos from "./components/Todos";
+import Todo from "./models/todo";
 
 function App() {
+  const todos = [new Todo("TypeScript"), new Todo("React")];
   return (
     <div>
-      <Todos items={['TypeScript', 'React']}/>
+      <Todos items={todos} />
     </div>
   );
 }
