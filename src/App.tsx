3ed3a2b5e4dea531055bd7caa86a3233b51176b1
strangelove <merkwurdigliebe0707@gmail.com>
2022-05-24 16:25:42 +0300

    rprops were replaced by context

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,13 @@
-import { useState } from "react";
-
-import "./App.css";
-import Todos from "./components/Todos";
-import Todo from "./models/todo";
-import NewTodo from "./components/NewTodo";
+import NewTodo from './components/NewTodo';
+import Todos from './components/Todos';
+import TodosContextProvider from './store/todos-context';
 
 function App() {
-  // const todos = [new Todo("TypeScript"), new Todo("React")];
-  const [todos, setTodos] = useState<Todo[]>([]);
-
-
-  const addTodoHandler = (todoText: string) => {
-    const newTodo = new Todo(todoText);
-    setTodos((prevTodos) => {
-      return prevTodos.concat(newTodo);
-    });
-  };
-
-  const removeTodoHandler = (todoId: string) => {
-    console.log('is triggered!');
-    setTodos((prevTodos) => {
-      return prevTodos.filter(todo => todo.id !== todoId)
-    });
-  };
-
   return (
-    <div>
-      <NewTodo onAddTodo={addTodoHandler} />
-      <Todos onRemoveTodo={removeTodoHandler} items={todos} />
-    </div>
+    <TodosContextProvider>
+      <NewTodo />
+      <Todos />
+    </TodosContextProvider>
   );
 }
 
